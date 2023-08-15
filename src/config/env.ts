7f696d196ae795fad4fbf86f323e3d83f8627a80
Yume <hei.hajatiana@gmail.com>
2023-08-15 21:27:30 +0300

    build(env): setup .env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const SCRAPING_DOG_API_KEY = process.env.SCRAPING_DOG_API_KEY as string;
--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,8 +1,7 @@
-import { SCRAPING_DOG_API_KEY } from "./env";
+import {SCRAPING_DOG_API_KEY} from './env';
 
 export const createScrapingDogUrl = (url: string) =>
-  `https://api.scrapingdog.com/scrape?api_key=${SCRAPING_DOG_API_KEY}&url=${url}`
-
+  `https://api.scrapingdog.com/scrape?api_key=${SCRAPING_DOG_API_KEY}&url=${url}`;
 
 export const indeedUrl = (q: string) =>
   `https://www.indeed.com/jobs?q=${encodeURI(q)}&l=&vjk=e7b930755e7c9bdc&dynamic=false`;
