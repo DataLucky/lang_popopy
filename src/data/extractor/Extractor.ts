a45223dfeed269341a1604a84d29705d7609919c
Yume <hei.hajatiana@gmail.com>
2023-08-15 21:42:49 +0300

    chore(extractor): allow input reset for better batch extraction

--- src/data/extractor/Extractor.ts
+++ src/data/extractor/Extractor.ts
@@ -2,13 +2,20 @@ import {CheerioAPI, load} from 'cheerio';
 
 export interface Extractor<TResult> {
   extract(): TResult[];
+
+  reset(input: string): this;
 }
 
 export abstract class BaseExtractor<TResult = any> implements Extractor<TResult> {
   protected _$: CheerioAPI;
 
-  constructor(protected readonly text: string) {
-    this._$ = load(text);
+  constructor(protected readonly input: string) {
+    this._$ = load(input);
+  }
+
+  reset(input: string) {
+    this._$ = load(input);
+    return this;
   }
 
   abstract extract(): TResult[];
